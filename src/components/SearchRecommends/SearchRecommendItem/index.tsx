34c0de86af9014e591160c220d7a01d4cac526a5
Jung Su <wjdwjdtn92@naver.com>
2023-05-04 03:09:25 +0900

    [Feat] 선택한 index인 경우 background가 변경되도록 수정

--- src/components/SearchRecommends/SearchRecommendItem/index.tsx
+++ src/components/SearchRecommends/SearchRecommendItem/index.tsx
@@ -3,9 +3,22 @@ import { SearchRecommendType } from '@/types/recommend.type';
 import { ReactComponent as SearchIcon } from '@icons/search-icon.svg';
 import styles from './SearchRecommendItem.module.css';
 
-function SearchRecommendItem({ id, name }: SearchRecommendType) {
+interface SearchRecommendItemProps extends SearchRecommendType {
+  isSeleted: boolean;
+}
+
+function SearchRecommendItem({
+  id,
+  name,
+  isSeleted,
+}: SearchRecommendItemProps) {
+  const className = isSeleted
+    ? [styles.item, styles.selected].join(' ')
+    : styles.item;
+
+  console.log(className);
   return (
-    <li className={styles.item} id={`recommend_id_${id}`}>
+    <li className={className} id={`recommend_id_${id}`}>
       <SearchIcon className={styles.search_icon} />
       {name}
     </li>
--- src/components/SearchRecommends/index.tsx
+++ src/components/SearchRecommends/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import SearchRecommendItem from './SearchRecommendItem';
 import SearchRecommendList from './SearchRecommendList';
 import { SearchRecommendType } from '@/types/recommend.type';
@@ -6,19 +6,28 @@ import styles from './SearchRecommend.module.css';
 
 interface SearchRecommendProps {
   searchRecommendList: SearchRecommendType[];
+  selectedIndex: number;
 }
 
-function SearchRecommends({ searchRecommendList }: SearchRecommendProps) {
-  return searchRecommendList.length > 0 ? (
+function SearchRecommends({
+  searchRecommendList,
+  selectedIndex,
+}: SearchRecommendProps) {
+  return (
     <div className={styles.container}>
       <h2 className={styles.title}>추천검색어</h2>
       <SearchRecommendList>
-        {searchRecommendList.map(({ id, name }) => (
-          <SearchRecommendItem key={id} id={id} name={name} />
+        {searchRecommendList.map(({ id, name }, index) => (
+          <SearchRecommendItem
+            key={id}
+            id={id}
+            name={name}
+            isSeleted={selectedIndex === index}
+          />
         ))}
       </SearchRecommendList>
     </div>
-  ) : null;
+  );
 }
 
 export default SearchRecommends;
